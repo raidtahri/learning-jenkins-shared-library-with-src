b9c3fee7b745559023d5aea73a6b0b25088be0d6
raidtahri <220400166+raidtahri@users.noreply.github.com>
2025-07-26 20:39:16 +0100

    fix(vars/buildApp.groovy): set working directory to locate pom.xml

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,6 +1,7 @@
 #!/usr/bin/env groovy
 def call() {
-
+           dir('07-hello-maven-shared-library-with-scr-pipeline') {
                 echo 'Building the app with Maven'
                 sh 'mvn clean package -DskipTests'
                 }
+}
