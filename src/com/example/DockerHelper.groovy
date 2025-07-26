15d86fcf768c1334b31babea800ba8c90aa92d3c
raidtahri <220400166+raidtahri@users.noreply.github.com>
2025-07-26 19:05:27 +0100

    feat(src/com/example/DockerHelper.groovy): update DockerHelper file with build and push methods

---
+++ src/com/example/DockerHelper.groovy
@@ -0,0 +1,41 @@
+package com.example
+
+class DockerHelper implements Serializable {
+
+    def steps
+    String dockerDir
+
+    DockerHelper(steps, String dockerDir = '.') {
+        this.steps = steps
+        this.dockerDir = dockerDir
+    }
+
+    def buildImage(String imageName, String imageTag) {
+        steps.dir(dockerDir) {
+            steps.withCredentials([steps.usernamePassword(
+                credentialsId: 'dockerhub',
+                usernameVariable: 'DOCKER_USER',
+                passwordVariable: 'DOCKER_PASS'
+            )]) {
+                steps.echo "Building Docker image: ${imageName}:${imageTag}"
+                steps.sh "docker build -t ${steps.env.DOCKER_USER}/${imageName}:${imageTag} ."
+            }
+        }
+    }
+
+    def pushImage(String imageName, String imageTag) {
+        steps.withCredentials([steps.usernamePassword(
+            credentialsId: 'dockerhub',
+            usernameVariable: 'DOCKER_USER',
+            passwordVariable: 'DOCKER_PASS'
+        )]) {
+            steps.echo "Pushing Docker image to Docker Hub"
+            steps.sh """
+                echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+                docker push ${steps.env.DOCKER_USER}/${imageName}:${imageTag}
+                docker logout
+            """
+        }
+    }
+}
+
