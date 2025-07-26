b84ad4a82a9d6ede0f3fc9f2319fdebb22b2debf
raidtahri <220400166+raidtahri@users.noreply.github.com>
2025-07-26 20:56:51 +0100

    fix(vars/buildDockerImage.groovy): set working directory to locate Dockerfile

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -2,6 +2,6 @@
 import com.example.DockerHelper
 
 def call(String imageName, String imageTag) {
-    def helper = new DockerHelper(this)
+    def helper = new DockerHelper(this, '07-hello-maven-shared-library-with-scr-pipeline')
     helper.buildDockerImage(imageName, imageTag)
 }
